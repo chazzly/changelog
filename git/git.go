c0595df4e33b53baca0e3c8ccbd678b86235c9f4
cruhl <cruhl@cars.com>
2016-08-08 15:03:35 -0500

    Cleaned up Parsing of git commits. Commented out logic for commit "lables", not doing this at the moment.

--- git/git.go
+++ git/git.go
@@ -2,11 +2,11 @@ package git
 
 import (
 	"os/exec"
-	"regexp"
+//	"regexp"
 	"fmt"
-	"strconv"
+//	"strconv"
 	"strings"
-	"unicode/utf8"
+//	"unicode/utf8"
 //	"github.com/codegangsta/cli"
 )
 
@@ -14,11 +14,11 @@ const gitCommitGrep = "^feat|^fix|BREAKING"
 const gitCommitFormat = "%H%n%cd%n%an%n%s%n%b%n==END=="
 const MAX_SUBJECT_LENGTH = 80
 
-var closeRegex = regexp.MustCompile(`\s*(?:Closes|Fixes|Resolves)\s#(\d+)`)
-var allTypesRegex = regexp.MustCompile(`(?:Closes|Fixes|Resolves)\s((?:#\d+(?:\,\s)?)+)`)
-var numberRegex = regexp.MustCompile(`\d+`)
-var breakingRegex = regexp.MustCompile(`/BREAKING CHANGE:\s([\s\S]*)/ig`)
-var commitPatternRegex = regexp.MustCompile(`^(\w*)(\(([\w\$\.\-\* ]*)\))?\: (.*)$`)
+// var closeRegex = regexp.MustCompile(`\s*(?:Closes|Fixes|Resolves)\s#(\d+)`)
+// var allTypesRegex = regexp.MustCompile(`(?:Closes|Fixes|Resolves)\s((?:#\d+(?:\,\s)?)+)`)
+// var numberRegex = regexp.MustCompile(`\d+`)
+// var breakingRegex = regexp.MustCompile(`/BREAKING CHANGE:\s([\s\S]*)/ig`)
+// var commitPatternRegex = regexp.MustCompile(`^(\w*)(\(([\w\$\.\-\* ]*)\))?\: (.*)$`)
 
 func GetLatestTag() (tag string, err error) {
 	hashCmd, err := exec.Command("git", "rev-list", "--tags", "--max-count=1").Output()
@@ -64,7 +64,6 @@ type Commit struct {
 func GetChangelogCommits(from string, to string) (commits []*Commit, err error) {
 	commits = make([]*Commit, 0)
 	commitRange := from + ".." + to
-	fmt.Println(commitRange)
 	from = strings.TrimPrefix(from, "\n")
 	from = strings.TrimSuffix(from, "\n")
 	to = strings.TrimPrefix(to, "\n")
@@ -73,6 +72,7 @@ func GetChangelogCommits(from string, to string) (commits []*Commit, err error) 
 	gitCmd := exec.Command("git", "log", "--format=\"" + gitCommitFormat + "\"", "--date=format:%c", commitRange)
 	output, err := gitCmd.Output()
 	if err != nil {
+		fmt.Println("ERROR getting LOG")
 		return
 	}
 
@@ -89,7 +89,7 @@ func parseCommit(commit string, commits []*Commit) []*Commit {
 		return commits
 	}
 
-	commit = strings.TrimPrefix(commit, "\n")
+	commit = strings.Trim(commit, "\"\n")
 	lines := strings.Split(commit, "\n")
 
 	if len(lines) < 3 {
@@ -97,62 +97,62 @@ func parseCommit(commit string, commits []*Commit) []*Commit {
 	}
 
 	newCommit := &Commit{}
-	newCommit.Hash = strings.Replace(lines[1], "\n", "", 0)
-	newCommit.Author = strings.Replace(lines[3], "\n", "", 0)
-	newCommit.Date = strings.Replace(lines[2], "\n", "", 0)
-	newCommit.Subject = strings.Replace(lines[4], "\n", "", 0)
+	newCommit.Hash = strings.Replace(lines[0], "\n", "", 0)
+	newCommit.Author = strings.Replace(lines[2], "\n", "", 0)
+	newCommit.Date = strings.Replace(lines[1], "\n", "", 0)
+	newCommit.Subject = strings.Replace(lines[3], "\n", "", 0)
 	newCommit.Closes = make([]int, 0)
 	newCommit.Breaks = make([]string, 0)
 	newCommit.Body = commit
 
-	// remove all issue numbers in subject
-	closeRegex.ReplaceAllFunc([]byte(newCommit.Subject), func(s []byte) []byte {
-		no, err := strconv.Atoi(string(s))
-		if err != nil {
-			return s
-		}
-
-		newCommit.Closes = append(newCommit.Closes, no)
-		return []byte("")
-	})
-
-	// find all closes/fixes numbers in commit message
-	for _, line := range lines {
-		matches := allTypesRegex.FindAllString(line, -1)
-		for _, match := range matches {
-			for _, msg := range strings.Split(match, ",") {
-				if numberRegex.MatchString(msg) {
-					submatches := numberRegex.FindStringSubmatch(msg)
-					if len(submatches) > 0 {
-						number, _ := strconv.Atoi(submatches[0])
-						newCommit.Closes = append(newCommit.Closes, number)
-					}
-				}
-			}
-		}
-	}
-
-	// find all breaking changes
-	if matches := breakingRegex.FindStringSubmatch(commit); len(matches) > 1 {
-		newCommit.Breaks = append(newCommit.Breaks, matches[1]+"\n")
-	}
-
-	// find component, type and subject
-	submatches := commitPatternRegex.FindStringSubmatch(newCommit.Subject)
-
-	if len(submatches) < 5 {
-		return commits
-	}
-
-	// shorten subject
-	if utf8.RuneCountInString(submatches[4]) > 80 {
-		submatches[4] = runeSubstr(submatches[4], 0, 80)
-	}
-
-	newCommit.Type = submatches[1]
-	newCommit.Component = submatches[3]
-	newCommit.Subject = submatches[4]
-
+		// remove all issue numbers in subject
+	//closeRegex.ReplaceAllFunc([]byte(newCommit.Subject), func(s []byte) []byte {
+	//	no, err := strconv.Atoi(string(s))
+	//	if err != nil {
+	//		return s
+	//	}
+	//
+	//	newCommit.Closes = append(newCommit.Closes, no)
+	//	return []byte("")
+	//})
+	//
+	//// find all closes/fixes numbers in commit message
+	//for _, line := range lines {
+	//	matches := allTypesRegex.FindAllString(line, -1)
+	//	for _, match := range matches {
+	//		for _, msg := range strings.Split(match, ",") {
+	//			if numberRegex.MatchString(msg) {
+	//				submatches := numberRegex.FindStringSubmatch(msg)
+	//				if len(submatches) > 0 {
+	//					number, _ := strconv.Atoi(submatches[0])
+	//					newCommit.Closes = append(newCommit.Closes, number)
+	//				}
+	//			}
+	//		}
+	//	}
+	//}
+	//
+	//// find all breaking changes
+	//if matches := breakingRegex.FindStringSubmatch(commit); len(matches) > 1 {
+	//	newCommit.Breaks = append(newCommit.Breaks, matches[1]+"\n")
+	//}
+	//
+	//// find component, type and subject
+	//submatches := commitPatternRegex.FindStringSubmatch(newCommit.Subject)
+	//
+	//if len(submatches) < 5 {
+	//	return commits
+	//}
+	//
+	//// shorten subject
+	//if utf8.RuneCountInString(submatches[4]) > 80 {
+	//	submatches[4] = runeSubstr(submatches[4], 0, 80)
+	//}
+	//
+	//newCommit.Type = submatches[1]
+	//newCommit.Component = submatches[3]
+	//newCommit.Subject = submatches[4]
+	//
 	return append(commits, newCommit)
 }
 
